0ba945349e806c60bc6e9a8096aab74088e89de3
Michael <mayrho230008@sr.htlweiz.com>
2025-11-17 08:23:33 +0100

    Add relative func for family tree and enum created

--- StammbaumCode/src/FamilyTree.cs
+++ StammbaumCode/src/FamilyTree.cs
@@ -1,11 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FamilyTree.Models;
 
 public class FamilyTree
 {
-    private Decedent Personfortree_;
+    private Decedent _personfortree;
 
-    private List<Relative> _relative;
+    enum Relationtypes
+    {
+        Son,
+        Daughter,
+        Mother,
+        Father,
 
 
+    }
+
+    private Dictionary<Relationtypes, Relative> _relative = new Dictionary<Relationtypes, Relative>();
+
+    public FamilyTree(Decedent person)
+    {
+        _personfortree = person;
+    }
+
+    public void AddRelative(Relative elative, string type)
+    {
+
+        if (Enum.TryParse(typeof(Relationtypes), type, true, out var result))
+        {
+            Relationtypes relationType = (Relationtypes)result;
+
+            _relative.Add(relationType, elative);
+        }
+        else
+        {
+            Console.WriteLine($"Invalid relation type: {type}");
+        }
+    }
+
+
+    public void Dissplay()
+    {
+        
+
+        
+    }
 
 }
+
+
--- StammbaumCode/src/obj/Debug/net9.0/FamilyTree.Models.AssemblyInfo.cs
+++ StammbaumCode/src/obj/Debug/net9.0/FamilyTree.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyTree.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3cffceae52e07437fc7599082128dd65940312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975564e0b93886518ecaf938e824d739742d06e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyTree.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyTree.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
