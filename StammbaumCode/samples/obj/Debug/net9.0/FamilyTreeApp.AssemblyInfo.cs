5e852e5365503baecd8d72480ab854cdd677b7e6
Michael <mayrho230008@sr.htlweiz.com>
2025-12-01 08:08:26 +0100

    Webapp.xaml

--- StammbaumCode/samples/obj/Debug/net9.0/FamilyTreeApp.AssemblyInfo.cs
+++ StammbaumCode/samples/obj/Debug/net9.0/FamilyTreeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51c2f4f4b9ef535b713c6cd6c2ba396b707e9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ffbaec01fd657704fbf05db3763e1d41789e509")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
